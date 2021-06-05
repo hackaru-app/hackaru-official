099713d5eed8e436e2b010bda81c85a472ca3a7b
ktmouk <ktmouk@gmail.com>
2021-06-05 16:17:37 +0900

    Add message of privacy policy

---
+++ components/organisms/latest-news.vue
@@ -0,0 +1,42 @@
+<i18n src="~/assets/locales/components/organisms/latest-news.json"></i18n>
+
+<template>
+  <section class="latest-news">
+    <a
+      href="https://github.com/hackaru-app/hackaru-terms/blob/master/privacy-policy.md"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <icon name="volume-2-icon" class="news-icon" />{{ $t('date') }}
+      {{ $t('message') }}
+    </a>
+  </section>
+</template>
+
+<script>
+import Icon from '~/components/atoms/icon'
+
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.latest-news {
+  border-bottom: 1px solid #2e3647;
+  text-align: center;
+  padding: 10px 0;
+  font-size: 14px;
+}
+.news-icon {
+  margin-right: 10px;
+}
+</style>
